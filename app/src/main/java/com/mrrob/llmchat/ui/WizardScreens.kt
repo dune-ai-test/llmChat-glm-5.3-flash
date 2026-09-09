5d5af331aa0c144b9e8c515d92c0c004342f032e
dune <duneaitest@gmail.com>
2026-09-09 20:29:27 +0530

    Fix imports placed above package declaration in WizardScreens

--- app/src/main/java/com/mrrob/llmchat/ui/WizardScreens.kt
+++ app/src/main/java/com/mrrob/llmchat/ui/WizardScreens.kt
@@ -1,7 +1,7 @@
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.AlertDialog
-import androidx.compose.ui.graphics.SolidColor
 package com.mrrob.llmchat.ui
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.TextButton
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
